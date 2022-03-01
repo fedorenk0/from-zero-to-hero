2821122bca98b23275fa4abe218052aa89ab227c
fedorenko <fedorenko.sanya.98@gmail.com>
2022-03-01 21:23:10 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,34 @@
+"use sctrict";
+/* 
+/* a = 15;
+console.log(a);
+
+let number = 5;
+const leftBorderWidht = 1;
+ 
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+
+console.log(name);
+var name = 'Ivan';
+
+{
+    let result = 50;
+}
+
+alert(5);
+
+const result = confirm("Are you here?");
+console.log(result); */
+
+
+
