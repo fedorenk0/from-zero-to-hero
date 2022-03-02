6482abf9e364a587136f52d6296b55ec71085942
fedorenko <fedorenko.sanya.98@gmail.com>
2022-03-02 18:00:20 +0200

    New programming language

--- js/script.js
+++ js/script.js
@@ -1,34 +1,72 @@
-"use sctrict";
-/* 
-/* a = 15;
-console.log(a);
+"use strict";
 
-let number = 5;
-const leftBorderWidht = 1;
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidht = 1;
  
-number = 10;
-console.log(number);
+// number = 10;
+// console.log(number);
 
-const obj = {
-    a: 50
-};
+// const obj = {
+//     a: 50
+// };
 
-obj.a = 10;
+// obj.a = 10;
 
-console.log(obj);
+// console.log(obj);
 
+// console.log(name);
+// var name = 'Ivan';
 
-console.log(name);
-var name = 'Ivan';
+// {
+//     let result = 50;
+// }
 
-{
-    let result = 50;
-}
+// alert(5);
 
-alert(5);
+// const result = confirm("Are you here?");
+// console.log(result);
 
-const result = confirm("Are you here?");
-console.log(result); */
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
 
+// const answers = [];
 
+// answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+
+// console.log(`http://someurl.com/${category}/5`);
+
+// const user = "Ivan"
+
+// alert(`Привет, ${user}`);
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%2);
+
+    console.log(2 + 2 * 2 !== '6');
+
+    const isChecked = true,
+          isClose = false;
+
+    console.log(isChecked || !isClose);
 
